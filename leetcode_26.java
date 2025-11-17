e2c230a4ff668f1bce4c54777a48bd6f0c81a23a
hiabhii <125079006+abhishek3340@users.noreply.github.com>
2025-11-18 01:31:32 +0530

    Added Queston

--- leetcode_26.java
+++ leetcode_26.java
@@ -1,9 +1,14 @@
-public class leetcode_26 {
+//leetcode_26 
+public int removeDuplicates(int[] nums){
     
-    int n = nums.length;
-    int j=1;
-    for(int i=0;i<n;i++){{
-
+    int n = nums.length;//array
+    int j=1;//j pointer start from 1 because first element is always unique
+    for(int i=0;i<n;i++){  //
+        if(nums[i]!= nums[j-1]){
+            nums[j]= nums[i];
+            j++;
+        }
+    return j;
 
     }
 }
